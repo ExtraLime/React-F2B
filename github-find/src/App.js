e9eec4dd72a4f7cba373a975b66f707f10740b20
ExtraLime <willdox7@live.com>
2020-10-06 22:19:46 -0700

    added clear button

--- github-find/src/App.js
+++ github-find/src/App.js
@@ -11,28 +11,28 @@ class App extends Component {
     users:[],
     loading:false
   }
-
-  // async componentDidMount() {
-  //   this.setState({loading:true});
-  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-  //   console.log(res.data)
-  //   this.setState({users:res.data, loading:false});
-  // }
+  
   //search Github users
   searchUsers = async text => {
+    this.setState({ loading: true })
     console.log(text);
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
     this.setState({users: res.data.items, loading: false})
   }
 
+  //clear user results
+
+  clearUsers = () => { this.setState( {users:[],loading:false})}
+
   render() {
+    const { users, loading  } = this.state;
     
     return(
       <div className="App">
         < Navbar title='Github Finder' icon="fab fa-github"/>
         <div className="container">
-          <Search searchUsers={this.searchUsers}/>
-          <Users loading={this.state.loading} users={this.state.users}/>
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false}/>
+          <Users loading={loading} users={users}/>
         </div>        
       </div>
     )
--- github-find/src/components/users/Search.js
+++ github-find/src/components/users/Search.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 
 export class Search extends Component {
 
     state = {
         text: ''
+    };
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
+
     };
 
     onChange = (e) => this.setState({text:e.target.value});
@@ -15,6 +24,8 @@ export class Search extends Component {
     }
 
     render() {
+
+        const { showClear, clearUsers } = this.props;
         return (
             <div>
                 <form onSubmit={this.onSubmit} className="form">
@@ -28,6 +39,8 @@ export class Search extends Component {
                            value='Search'
                            className='btn btn-dark btn-block' />
                 </form>
+                {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button> }
+                
             </div>
         )
     }
