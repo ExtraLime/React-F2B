07d2c58677366dda79b2feb7b122427a188822f9
ExtraLime <willdox7@live.com>
2020-10-06 23:22:34 -0700

    added react router and set up about page

---
+++ github-find/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <>
+            <h1 className="">About This App</h1>
+            <p className="">App to search Github users</p>
+            <p className="">version 1.0.0</p>
+        </>
+    )
+}
+export default About
