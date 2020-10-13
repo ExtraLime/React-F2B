d98f932c4a54eed8c14cc09f6da76c1df7fa5a4a
ExtraLime <willdox7@live.com>
2020-10-13 13:12:27 -0700

    added loaduser and login user working on log out and conditional display on home page

--- contact-keeper/client/src/components/auth/Register.js
+++ contact-keeper/client/src/components/auth/Register.js
@@ -2,19 +2,24 @@ import React, { useState, useContext, useEffect } from "react";
 import AlertContext from "../../context/alert/alertContext";
 import AuthContext from "../../context/auth/authContext";
 
-const Register = () => {
+const Register = (props) => {
   const alertContext = useContext(AlertContext);
   const authContext = useContext(AuthContext);
 
-  const { register, error, clearErrors } = authContext
+  const { register, error, clearErrors, isAuthenticated } = authContext;
   const { setAlert } = alertContext;
 
   useEffect(() => {
-      if(error === 'User already exists') {
-          setAlert(error, 'danger');
-          clearErrors();
-      }
-  }, [error])
+    if (isAuthenticated) {
+      props.history.push("/");
+    }
+
+    if (error === "User already exists") {
+      setAlert(error, "danger");
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
 
   const [user, setUser] = useState({
     name: "",
@@ -34,9 +39,9 @@ const Register = () => {
       setAlert("Passwords must match", "danger");
     } else {
       register({
-          name,
-          email,
-          password
+        name,
+        email,
+        password,
       });
     }
   };
--- contact-keeper/client/src/context/auth/AuthState.js
+++ contact-keeper/client/src/context/auth/AuthState.js
@@ -1,92 +1,117 @@
-import React, { useReducer } from 'react';
-import axios from 'axios'
-import AuthContext from './authContext'
-import authReducer from './authReducer';
+import React, { useReducer } from "react";
+import axios from "axios";
+import AuthContext from "./authContext";
+import authReducer from "./authReducer";
+import setAuthToken from '../../utils/setAuthToken'
 import {
-    REGISTER_SUCCESS,
-    REGISTER_FAIL,
-    USER_LOADED,
-    AUTH_ERROR,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL,
-    LOGOUT,
-    CLEAR_ERRORS
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS,
+} from "../types";
 
-} from '../types'
+const AuthState = (props) => {
+  const initialState = {
+    token: localStorage.getItem("token"),
+    user: null,
+    isAuthenticated: null,
+    loading: true,
+    error: null,
+  };
 
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
-const AuthState = props => {
-    const initialState = {
-      token: localStorage.getItem('token'),
-      user: null,
-      isAuthenticated: null,
-      loading: true,
-      error: null
+  // Load User
+  const loadUser = async () => {
+    if(localStorage.token){
+        setAuthToken(localStorage.token)
+    }
+    try {
+      const res = await axios.get("/api/auth");
+      dispatch({ type: USER_LOADED, payload: res.data });
+    } catch (error) {
+        dispatch({type: AUTH_ERROR})
+    }
+  };
+
+  // Register User
+  const register = async (formData) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
+    try {
+      const res = await axios.post("/api/users", formData, config);
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data,
+      });
 
-    const [ state, dispatch ] = useReducer(authReducer,initialState);
+      loadUser();
+    } catch (error) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: error.response.data.msg,
+      });
+    }
+  };
 
-   // Load User
-   const loadUser = () => {
-       console.log('load user')
-   }
-
-   // Register User
-   const register = async formData => {
-       const config = {
-           headers: {
-               'Content-Type': 'application/json'
-           }
-       }
-       try {
-           const res = await axios.post('/api/users', formData, config);
-           dispatch({
-               type: REGISTER_SUCCESS,
-               payload: res.data
-           })
-           
-       } catch (error) {
+  // Login User
+  const login = async (formData) => {
+    const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      try {
+        const res = await axios.post("/api/auth", formData, config);
         dispatch({
-            type: REGISTER_FAIL,
-            payload: error.response.data.msg
-        })
-           
-       }
+          type: LOGIN_SUCCESS,
+          payload: res.data,
+        });
+  
+        loadUser();
+      } catch (error) {
+        dispatch({
+          type: LOGIN_FAIL,
+          payload: error.response.data.msg,
+        });
+      }
+  };
 
-   }
+  // Logout
+  const logout = () => {
+    console.log("log out user");
+  };
 
-   // Login User
-   const login = () => {
-    console.log('log inuser')
-}
+  // Clear Errors
+  const clearErrors = () => {
+    dispatch({ type: CLEAR_ERRORS });
+  };
 
-   // Logout
-   const logout = () => {
-    console.log('log out user')
-}
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error,
+        register,
+        login,
+        logout,
+        clearErrors,
+        loadUser,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
-   // Clear Errors
-   const clearErrors = () => {
-    dispatch({ type: CLEAR_ERRORS})
-}
-
-    return (
-        <AuthContext.Provider
-        value={{
-            token: state.token,
-            isAuthenticated: state.isAuthenticated,
-            loading: state.loading,
-            user: state.user,
-            error: state.error,
-            register,
-            login,
-            logout,
-            clearErrors,
-            loadUser
-        }}>
-            {props.children}
-        </AuthContext.Provider>
-    )
-}
-
-export default AuthState
+export default AuthState;
