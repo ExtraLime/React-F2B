b1c1b8b2814afc92a735bd6329cb5e6f5a97c45c
ExtraLime <willdox7@live.com>
2020-10-14 22:46:50 -0700

    almost finished with it-logger, adding edit functionality

--- it-logger/src/components/layout/SearchBar.js
+++ it-logger/src/components/layout/SearchBar.js
@@ -7,8 +7,8 @@ const SearchBar = () => {
           <form>
             <div className="input-field">
               <input id="search" type="search" required />
-              <label className="label-icon" htmlFor="search"><i class="material-icons">search</i></label>
-              <i class="material-icons">close</i>
+              <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
+              <i className="material-icons">close</i>
             </div>
           </form>
         </div>
@@ -16,4 +16,4 @@ const SearchBar = () => {
     )
 }
 
-export default SearchBar
+export default SearchBar;
